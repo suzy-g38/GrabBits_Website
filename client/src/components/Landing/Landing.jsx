a5772306c04ded650ed11d052308519a9ea8c655
BK <bhushankale1025@gmail.com>
2023-01-30 02:17:52 +0530

    Testimonials frontend created

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import Highlights from '../Highlights/Highlights';
 import classes from './Landing.module.css';
 import Faq from '../Faq/Faq';
+import Testimonial from '../Testimonials/Testimonial';
+
 
 const Landing = () => {
 	return (
 		<>
 			<div className={classes.landing_bg}>
-				<Highlights />
-				<Faq />
+				{/* <Highlights /> */}
+				{/* <Faq /> */}
+				<Testimonial />
 			</div>
 		</>
 	);
