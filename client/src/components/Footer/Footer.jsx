7432587caa793c4bc030aaeeb6af951894d23309
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-01-31 00:53:08 +0530

    footer done

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,23 +1,46 @@
 import React from 'react';
 import classes from './Footer.module.css';
 import img from '../../assets/Logo.png';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
 	return (
 		<>
-			{/* <div className={classes.container}>
-				<div className={`${classes.container_subitem1}`}>
-				  <div className={classes.brand_logo}><img src={img} alt="logo" className={classes.logo}/></div>
-                  <span className={classes.brand_title}>GrabBit</span>
+			<div className={classes.outer_box}>
+				<div className={classes.row1}>
+					<NavLink to="/">
+						<div className={classes.brand}>
+							<div className={classes.brand_logo}>
+								<img src={img} alt="logo" className={classes.logo} />
+							</div>
+							<span className={classes.brand_title}>GrabBit</span>
+						</div>
+					</NavLink>
+					<div className={classes.social}>
+						<h3 className={classes.social_text}>Find Us On Social Media:</h3>
+						<ul className={classes.social_links}>
+							<li className={classes.socia_link}>
+								<a className={classes.link} href=""></a>
+							</li>
+							<li className={classes.socia_link}>
+								<a className={classes.link} href=""></a>
+							</li>
+							<li className={classes.socia_link}>
+								<a className={classes.link} href=""></a>
+							</li>
+							<li className={classes.socia_link}>
+								<a className={classes.link} href=""></a>
+							</li>
+						</ul>
+					</div>
+					<div className={classes.text_mail}>
+						<h2 className={classes.text}>We&apos;re Always Happy To Help</h2>
+						<h2 className={classes.mail}>grabbitofficial@gmail.com</h2>
+					</div>
 				</div>
-				<div>
-					
-				</div>
-				<div>
-
-				</div>
-
-		   </div> */}
+				<div className={classes.separator}></div>
+				<div className={classes.row2}>Copyright © 2023 GrabBits</div>
+			</div>
 		</>
 	);
 };
