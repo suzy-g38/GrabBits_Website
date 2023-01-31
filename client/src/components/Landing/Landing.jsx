532b1ed89eddc29d87c697ba10ad44803956c43b
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-01-31 21:56:20 +0530

    Landing Page Minor Changes

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import classes from './Landing.module.css';
+import Grabby from '../common/Grabby/Grabby';
 
 const Landing = () => {
 	return (
-		<>
+		<>   
+			<div className={classes.landingPage}>
+				<Grabby/>
+				<div className={classes.element}></div>
+			</div>
+
 			<div className={classes.landing_bg}></div>
 		</>
 	);
