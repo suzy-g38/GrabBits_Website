157fe259231db6672045ced98fb906a8405cb29d
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-01-31 22:50:54 +0530

    fix

--- client/src/components/common/index.js
+++ client/src/components/common/index.js
@@ -4,3 +4,4 @@ export { default as Input } from './Input/Input';
 export { default as DotAnim } from './DotAnim/DotAnim';
 export { default as Ticket } from './Ticket/Ticket';
 export { default as Textarea } from './Textarea/Textarea';
+export { default as Grabby } from './Grabby/Grabby';
