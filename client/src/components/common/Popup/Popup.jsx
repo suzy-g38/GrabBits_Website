38499d97d74ae72a97ff085be4b3fce558a06442
Spider9480 <omgawandeofficial9834899149@gmail.com>
2023-02-14 04:06:44 +0530

    popup modified

--- client/src/components/common/Popup/Popup.jsx
+++ client/src/components/common/Popup/Popup.jsx
@@ -5,54 +5,59 @@ import whatsapp from '../../../assets/Socials/whatsapp.svg';
 import cross from '../../../assets/cross.svg';
 
 const Popup = () => {
-	const [isVisible, setIsVisible] = useState(false);
+	const [showPopup, setShowPopup] = useState(false);
 
 	useEffect(() => {
-		setTimeout(() => {
-			setIsVisible(true);
-		}, 5000); // 5000 milliseconds = 5 seconds
-	}, []); // Only run the useEffect once, on mount
+		const popupShown = localStorage.getItem('popupShown');
+		if (!popupShown) {
+			setTimeout(() => {
+				setShowPopup(true);
+				localStorage.setItem('popupShown', true);
+			}, 5000);
+		}
+	}, []);
 
-	if (!isVisible) {
-		return null;
+	function handleClose() {
+		setShowPopup(false);
 	}
-	const closePopup = () => {
-		setIsVisible(false);
-	};
 
 	return (
 		<>
-			<div className={classes.blur}>
-				<div className={classes.card}>
-					<img
-						className={classes.close}
-						onClick={() => {
-							closePopup();
-						}}
-						src={cross}
-						alt="close"
-					/>
-					<h2 className={classes.text}>Join Our Whatsapp Group</h2>
-					<h1 className={classes.subtext}>
-						Stay updated with all the opportunites.
-					</h1>
-					<div className={classes.btn_container}>
-						<a
-							href="https://chat.whatsapp.com/KBxP1M7GT7mCh4PORsfN0H"
-							target="blank"
-						>
-							<Button bgColor="#25D366">
-								<img
-									className={classes.btn_svg}
-									src={whatsapp}
-									alt="whatsapp"
-								/>
-								<span className={classes.btn_text}>Whatsapp</span>
-							</Button>
-						</a>
+			{showPopup && (
+				<>
+					<div className={classes.blur}>
+						<div className={classes.card}>
+							<img
+								className={classes.close}
+								onClick={() => {
+									handleClose();
+								}}
+								src={cross}
+								alt="close"
+							/>
+							<h2 className={classes.text}>Join Our Whatsapp Group</h2>
+							<h1 className={classes.subtext}>
+								Stay updated with all the opportunites.
+							</h1>
+							<div className={classes.btn_container}>
+								<a
+									href="https://chat.whatsapp.com/KBxP1M7GT7mCh4PORsfN0H"
+									target="blank"
+								>
+									<Button bgColor="#25D366">
+										<img
+											className={classes.btn_svg}
+											src={whatsapp}
+											alt="whatsapp"
+										/>
+										<span className={classes.btn_text}>Whatsapp</span>
+									</Button>
+								</a>
+							</div>
+						</div>
 					</div>
-				</div>
-			</div>
+				</>
+			)}
 		</>
 	);
 };
