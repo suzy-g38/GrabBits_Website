1b1a70550cfea2fdd97fd997efd515e2e68c9689
Sapna Kul <kulsapna2210002@gmail.com>
2023-03-14 16:47:01 +0530

    changed the css of 404 page

--- client/src/pages/FourOhFour/FourOhFour.jsx
+++ client/src/pages/FourOhFour/FourOhFour.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import './FourOhFour.css'
+import classes from './FourOhFour.module.css'
 import Homepage from '../Homepage'
 export const FourOhFour = () => {
   return (
-    <div id='oopss'>
-    <div id='error-text'>
+    <div className={classes.oopss}>
+    <div className={classes.errorText}>
         <img src="https://cdn.rawgit.com/ahmedhosna95/upload/1731955f/sad404.svg" alt="404"/>
         <span>404 PAGE</span>
-        <p className='pa'>
+        <p className={classes.pa}>
            . The page you were looking for could not be found</p>
-        <p className='pb'>
+        {/* <p className={classes.pb}>
             ... Back to previous page
-        </p>
-        <a href='{<Homepage/>}' className='back'>... Back to previous page</a>
+        </p> */}
+        <a href='../Homepage.jsx' className={classes.back}>... Back to previous page</a>
     </div>
 </div>
   )
