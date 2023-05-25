5acf83fb48740b87c3e524131687c19dd1b65491
Ayush Parui <paruiayush900@gmail.com>
2023-05-25 19:32:28 +0530

    Added Strict Query to DB

--- server/database/db.js
+++ server/database/db.js
@@ -4,6 +4,7 @@ import { Server } from "../utils/config.js";
 const connectDB = async () => {
   try {
     console.log(Server.MONGO_URI);
+    mongoose.set("strictQuery", true);
     const conn = await mongoose.connect(Server.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
