57de729984ec2846c14ea5c3afa85e3e07cb7487
Princeton D'souza <princeton2102@gmail.com>
2023-06-04 05:46:44 +0530

    Fixed bug : redirect to top of page #126

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,79 +1,94 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import img from '../../assets/Logo.png';
 import classes from './Navbar.module.css';
 
 const Navbar = () => {
-
 	const [open, setOpen] = useState(false);
 
 	const changeOpen = () => {
-		if (open) {
-			setOpen(false);
-		} else {
-			setOpen(true);
+		setOpen(!open);
+	};
+	const handleLinkClick = () => {
+		if (window.innerWidth < 800) {
+			window.scrollTo(0, 0);
 		}
-	}
+	};
 
 	return (
 		<>
 			<header className={classes.header}>
 				<nav className={classes.nav}>
-					<NavLink to='/'>
-
-						{!open &&
+					<NavLink to="/">
+						{!open && (
 							<div className={classes.brand}>
 								<div className={classes.brand_logo}>
 									<img src={img} alt="logo" className={classes.logo} />
 								</div>
 								<span className={classes.brand__title}>GrabBit</span>
 							</div>
-						}
-
+						)}
 					</NavLink>
 
-					<div className={!open ? `${classes.nav__menu} ${classes.menu__close}` : `${classes.nav__menu} ${classes.menu__open}`}>
+					<div
+						className={`${classes.nav__menu} ${
+							open ? classes.menu__open : classes.menu__close
+						}`}
+					>
 						<ul className={`${classes.nav__list} ${classes.grid}`}>
-							<NavLink to="/">
+							<NavLink to="/" onClick={handleLinkClick}>
 								<li className={classes.nav__item}>
 									<i className="uil uil-estate" id={classes.nav__icon}></i> Home
 								</li>
 							</NavLink>
-							<NavLink to="/opportunities">
+							<NavLink to="/opportunities" onClick={handleLinkClick}>
 								<li className={classes.nav__item}>
-									<i className="uil uil-bag" id={classes.nav__icon}></i> Opportunities
+									<i className="uil uil-bag" id={classes.nav__icon}></i>
+									Opportunities
 								</li>
 							</NavLink>
-							<NavLink to="/podcasts">
+							<NavLink to="/podcasts" onClick={handleLinkClick}>
 								<li className={classes.nav__item}>
-									<i className="uil uil-megaphone" id={classes.nav__icon}></i> Podcasts
+									<i className="uil uil-megaphone" id={classes.nav__icon}></i>
+									Podcasts
 								</li>
 							</NavLink>
-							<NavLink to="/team">
+							<NavLink to="/team" onClick={handleLinkClick}>
 								<li className={classes.nav__item}>
-									<i className="uil uil-users-alt" id={classes.nav__icon}></i> About us
+									<i className="uil uil-users-alt" id={classes.nav__icon}></i>
+									About us
 								</li>
 							</NavLink>
-							<NavLink to="/contact">
+							<NavLink to="/contact" onClick={handleLinkClick}>
 								<li className={classes.nav__item}>
-									<i className="uil uil-voicemail-rectangle" id={classes.nav__icon}></i> Contact
+									<i
+										className="uil uil-voicemail-rectangle"
+										id={classes.nav__icon}
+									></i>
+									Contact
 								</li>
 							</NavLink>
 						</ul>
-						{open &&
-							<i className="uil uil-times" id={classes.nav__close} onClick={() => changeOpen()}></i>
-						}
+						{open && (
+							<i
+								className="uil uil-times"
+								id={classes.nav__close}
+								onClick={changeOpen}
+							></i>
+						)}
 					</div>
 
-					{!open &&
-						<i className="uil uil-apps" id={classes.nav__toggle__icon} onClick={() => changeOpen()}></i>
-					}
-
-
+					{!open && (
+						<i
+							className="uil uil-apps"
+							id={classes.nav__toggle__icon}
+							onClick={() => changeOpen()}
+						></i>
+					)}
 				</nav>
 			</header>
 		</>
-	)
-}
+	);
+};
 
 export default Navbar;
