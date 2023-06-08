52d39fadbb2a7ee508c6eff285471e74f9db49f4
Suzy <sulagnaghosh52@gmail.com>
2023-06-08 20:17:06 +0530

    chore: change to sync with main

--- server/database/db.js
+++ server/database/db.js
@@ -1,38 +1,22 @@
-// import mongoose from 'mongoose'
-// import { Server } from '../utils/config.js'
-
-// const connectDB = async () => {
-//   try {
-//     console.log(Server.MONGO_URI)
-//     mongoose.set('strictQuery', true)
-//     const conn = await mongoose.connect('mongodb://127.0.0.1:27017/grabbit', {
-//       useUnifiedTopology: true,
-//       useNewUrlParser: true,
-//       family: 4,
-//     })
-//     console.log(`MongoDB connected: ${conn.connection.host}`)
-//   } catch (error) {
-//     console.log(`Error: ${error}`)
-//     process.exit(1)
-//   }
-// }
-
-// export default connectDB
-
 import mongoose from 'mongoose'
-// const dotenv = require("dotenv");
-// dotenv.config();
+import { Server } from '../utils/config.js'
 
-const connectDB = () => {
+const connectDB = async () => {
   try {
-    mongoose.connect('mongodb://localhost:27017/grabbit', () => {
-      console.log('CONNECTED TO MONGO DB DATABASE 🌐')
+    console.log(Server.MONGO_URI)
+    mongoose.set('strictQuery', true)
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/grabbit', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      family: 4,
     })
-
-    console.log(`MongoDB connected: `)
+    console.log(`MongoDB connected: ${conn.connection.host}`)
   } catch (error) {
     console.log(`Error: ${error}`)
+    process.exit(1)
   }
 }
 
 export default connectDB
+
+
