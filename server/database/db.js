bb3c7b824a8d31c3e2a68ad90295eea98a2c3352
Sulagna Ghosh <62666287+suzy-g38@users.noreply.github.com>
2023-06-14 01:19:27 +0530

    fix: mongodb connection update

--- server/database/db.js
+++ server/database/db.js
@@ -1,22 +1,21 @@
-import mongoose from 'mongoose'
-import { Server } from '../utils/config.js'
+import mongoose from "mongoose";
+import { Server } from "../utils/config.js";
 
 const connectDB = async () => {
   try {
-    console.log(Server.MONGO_URI)
-    mongoose.set('strictQuery', true)
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/grabbit', {
+    console.log(Server.MONGO_URI);
+    mongoose.set("strictQuery", true);
+    const conn = await mongoose.connect(Server.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
-      family: 4,
-    })
-    console.log(`MongoDB connected: ${conn.connection.host}`)
+    });
+    console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
-    console.log(`Error: ${error}`)
-    process.exit(1)
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
   }
-}
+};
 
-export default connectDB
+export default connectDB;
 
 
