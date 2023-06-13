3d0326400d260f64274f52e44925bd1fc36c50f0
Sulagna Ghosh <62666287+suzy-g38@users.noreply.github.com>
2023-06-14 01:24:47 +0530

    fix: remove mongodb import

--- server/index.js
+++ server/index.js
@@ -1,39 +1,37 @@
-import express from 'express'
-import cors from 'cors'
-import { Server } from './utils/config.js'
-import connectDB from './database/db.js'
-import { errorMiddleware, notFound } from './middleware/errorMiddleware.js'
+import express from "express";
+import cors from "cors";
+import { Server } from "./utils/config.js";
+import connectDB from "./database/db.js";
+import { errorMiddleware, notFound } from "./middleware/errorMiddleware.js";
 // Routes
-import jobRouter from './routes/job.routes.js'
-import contactRouter from './routes/contact.routes.js'
-import podcastRouter from './routes/podcast.routes.js'
-import userRouter from './routes/user.routes.js'
+import jobRouter from "./routes/job.routes.js";
+import contactRouter from "./routes/contact.routes.js";
+import podcastRouter from "./routes/podcast.routes.js";
+import userRouter from "./routes/user.routes.js";
 
-import mongoose from 'mongoose'
+const PORT = Server.PORT;
 
-const PORT = Server.PORT
-
-const app = express()
+const app = express();
 
 // DB connection
-connectDB()
+connectDB();
 
 // middlewares
-app.use(cors())
-app.use(express.json({ limit: '4mb' }))
-app.use(express.urlencoded({ extended: true }))
-app.use(errorMiddleware)
+app.use(cors());
+app.use(express.json({ limit: "4mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(errorMiddleware);
 // app.use(notFound);
 
 // Routers
-app.use('/job', jobRouter)
-app.use('/contact', contactRouter)
-app.use('/podcast', podcastRouter)
-app.use('/User', userRouter)
-app.use('/', (req, res) => {
-  res.send("Welcome to the server of GrabBit, Don't go ahead!")
-})
+app.use("/job", jobRouter);
+app.use("/contact", contactRouter);
+app.use("/podcast", podcastRouter);
+app.use("/User", userRouter);
+app.use("/", (req, res) => {
+  res.send("Welcome to the server of GrabBit, Don't go ahead!");
+});
 
 app.listen(PORT, () => {
-  console.log(`Server running in ${Server.NODE_ENV} mode on ${PORT}`)
-})
+  console.log(`Server running in ${Server.NODE_ENV} mode on ${PORT}`);
+});
