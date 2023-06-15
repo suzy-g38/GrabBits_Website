dfbfd6ff10d3c13b9af315b1cecf7c7c068aab78
Dunna Avinash <56786344+Avinash905@users.noreply.github.com>
2023-06-16 00:57:28 +0530

    added alt attributes

--- client/src/components/Extra/Extra.jsx
+++ client/src/components/Extra/Extra.jsx
@@ -13,13 +13,13 @@ const Extra = () => {
 				<div className={classes.wrapper}>
 					<div className={classes.col1}>
 						<div className={`${classes.box1} ${classes.hoverEffect}`}>
-							<img src={icon4} alt="" />
+							<img src={icon4} alt="opportunities" />
 							<h1 className={classes.number}>1.</h1>
 							<h4 className={classes.head}>Opportunities</h4>
 							<p className={classes.para}>Grab them..</p>
 						</div>
 						<div className={`${classes.box3} ${classes.hoverEffect}`}>
-							<img src={icon3} alt="" />
+							<img src={icon3} alt="motivation" />
 							<h1 className={classes.number}>3.</h1>
 							<h4 className={classes.head}>Motivation</h4>
 							<p className={classes.para}>Uphold it..</p>
@@ -27,14 +27,14 @@ const Extra = () => {
 					</div>
 					<div className={classes.col2}>
 					<div className={`${classes.box2} ${classes.hoverEffect}`}>
-							<img src={icon2} alt="" />
+							<img src={icon2} alt="podcasts" />
 							<h1 className={classes.number}>2.</h1>
 							<h4 className={classes.head}>Podcasts</h4>
 							<p className={classes.para}>Learn from them..</p>
 						</div>
 
 						<div className={`${classes.box4} ${classes.hoverEffect}`}>
-							<img src={icon1} alt="" />
+							<img src={icon1} alt="resources" />
 							<h1 className={classes.number}>4.</h1>
 							<h4 className={classes.head}>Resources</h4>
 							<p className={classes.para}>Use them..</p>
